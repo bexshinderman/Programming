ca80e344cf7712ffc6771031db18998b4e9e9205
unknown <shinrl1@student.op.ac.nz>
2019-07-01 01:06:15 +1200

    count + search

---
+++ threes.py
@@ -0,0 +1,10 @@
+cap = input("Enter a number: ")
+capnum = int(cap)
+n = 0;
+for i in range (1, capnum):
+    n = n+1
+    if "3" in str(n):
+        print ( n, "ouch")
+    else:
+        print (n)
+   
