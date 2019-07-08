57a772a38e126122b6a5280b6c5c5e9042e8a5f1
unknown <shinrl1@student.op.ac.nz>
2019-07-09 01:27:20 +1200

    code now working properly

--- 99bottles.py
+++ 99bottles.py
@@ -1,9 +1,14 @@
 a = input('Enter first num in range: ')
 b = input('Enter last num in range: ')
+a = int(a)
+b = int(b)
 
-for x in reversed(range(a,101)):
- 
-    print(str(x)+" bottles of weird on the wall ")
+
+for x in reversed(range(a,b+1)):
+    #x = int(x)
+    print(x, "bottles of weird on the wall ")
     if x == 1:
         print("...and now there's none :( ")
+    if x == 0:
+        exit()
     
